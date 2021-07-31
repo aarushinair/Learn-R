e4c4c00a055ed930d2cc09dacfdcebc6b97359d6
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:48:03 +0530

    Create userInput.r

---
+++ userInput.r
@@ -0,0 +1,21 @@
+#Take input from user
+my.name <- readline(prompt="Enter name: ")
+my.age <- readline(prompt="Enter age: ")
+# convert character into integer
+my.age <- as.integer(my.age)
+print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old."))
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
