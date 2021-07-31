80f92120995b9c79ef72d14b8454c84a0a4fef38
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:50:33 +0530

    Create population.r

---
+++ population.r
@@ -0,0 +1,24 @@
+#Sample From a Population
+#Sample 2 items from x
+
+> x
+[1]  1  3  5  7  9 11 13 15 17
+> # sample 2 items from x
+> sample(x, 2)
+[1] 13  9
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
