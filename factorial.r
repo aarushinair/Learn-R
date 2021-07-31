2d4a2f014d5003bb7aa853139d71c37b549bc233
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:56:03 +0530

    Create factorial.r

---
+++ factorial.r
@@ -0,0 +1,30 @@
+#Find the factorial of a number
+# take input from the user
+num = as.integer(readline(prompt="Enter a number: "))
+factorial = 1
+# check is the number is negative, positive or zero
+if(num < 0) {
+print("Sorry, factorial does not exist for negative numbers")
+} else if(num == 0) {
+print("The factorial of 0 is 1")
+} else {
+for(i in 1:num) {
+factorial = factorial * i
+}
+print(paste("The factorial of", num ,"is",factorial))
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
