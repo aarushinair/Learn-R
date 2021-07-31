22bb062f5eca3e99c279a1b8e1768fff5bebc85e
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:54:24 +0530

    Create MaxAndMin.r

---
+++ MaxAndMin.r
@@ -0,0 +1,28 @@
+#Find Minimum and Maximum
+> x
+[1]  5  8  3  9  2  7  4  6 10
+> # find the minimum
+> min(x)
+[1] 2
+> # find the maximum
+> max(x)
+[1] 10
+> # find the range
+> range(x)
+[1]  2 10
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
