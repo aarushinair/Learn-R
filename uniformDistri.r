4619e85a89e14e8de4c58da5566b592347b47e6f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:48:59 +0530

    Update uniformDistri.r

--- uniformDistri.r
+++ uniformDistri.r
@@ -5,3 +5,19 @@
 [1] 0.8090284 0.1797232 0.6803607
 > runif(3, min=5, max=10)    # define the range between 5 and 10
 [1] 7.099781 8.355461 5.173133
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
