8036a1cab9d9dfb6d30774b91bc55453521a3cf2
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:59:42 +0530

    Create leapYear.r

---
+++ leapYear.r
@@ -0,0 +1,31 @@
+#Check Leap Year
+# Program to check if the input year is a leap year or not
+year = as.integer(readline(prompt="Enter a year: "))
+if((year %% 4) == 0) {
+if((year %% 100) == 0) {
+if((year %% 400) == 0) {
+print(paste(year,"is a leap year"))
+} else {
+print(paste(year,"is not a leap year"))
+}
+} else {
+print(paste(year,"is a leap year"))
+}
+} else {
+print(paste(year,"is not a leap year"))
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
