1c9cbb348d282ec1658a3b943a3d4a186cd46d40
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 18:02:08 +0530

    Create naturalNumber.r

---
+++ naturalNumber.r
@@ -0,0 +1,29 @@
+#Find sum of natural numbers without formula
+# take input from the user
+num = as.integer(readline(prompt = "Enter a number: "))
+if(num < 0) {
+print("Enter a positive number")
+} else {
+sum = 0
+# use while loop to iterate until zero
+while(num > 0) {
+sum = sum + num
+num = num - 1
+}
+print(paste("The sum is", sum))
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
