ecdcd84b787151320846099120bafbb591540920
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:56:51 +0530

    Create multiplicationTable.r

---
+++ multiplicationTable.r
@@ -0,0 +1,23 @@
+#Multiplication Table
+# R Program to find the multiplicationtable (from 1 to 10)
+# take input from the user
+num = as.integer(readline(prompt = "Enter a number: "))
+# use for loop to iterate 10 times
+for(i in 1:10) {
+print(paste(num,'x', i, '=', num*i))
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
