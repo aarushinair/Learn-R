e8d6a01e407b4033d564abe8a5a39ade17fb8746
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 18:00:36 +0530

    Create oddEven.r

---
+++ oddEven.r
@@ -0,0 +1,24 @@
+Check Odd and Even Number
+# A number is even if division by 2 give a remainder of 0.
+# If remainder is 1, it is odd.
+num = as.integer(readline(prompt="Enter a number: "))
+if((num %% 2) == 0) {
+print(paste(num,"is Even"))
+} else {
+print(paste(num,"is Odd"))
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
