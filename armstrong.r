8efb500c66ba345f1bed078fbff0b753d8bfd3d6
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:58:11 +0530

    Create armstrong.r

---
+++ armstrong.r
@@ -0,0 +1,33 @@
+#Check Armstrong number
+# take input from the user
+num = as.integer(readline(prompt="Enter a number: "))
+# initialize sum
+sum = 0
+# find the sum of the cube of each digit
+temp = num
+while(temp > 0) {
+digit = temp %% 10
+sum = sum + (digit ^ 3)
+temp = floor(temp / 10)
+}
+# display the result
+if(num == sum) {
+print(paste(num, "is an Armstrong number"))
+} else {
+print(paste(num, "is not an Armstrong number"))
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
