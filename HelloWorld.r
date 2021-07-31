00c76405be31a80d41c29abbb04b75544a3ae673
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:44:48 +0530

    Create HelloWorld.r

---
+++ HelloWorld.r
@@ -0,0 +1,25 @@
+> # We can use the print() function
+> print("Hello World!")
+[1] "Hello World!"
+> # Quotes can be suppressed in the output
+> print("Hello World!", quote = FALSE)
+[1] Hello World!
+> # If there are more than 1 item, we can concatenate using paste()
+> print(paste("How","are","you?"))
+[1] "How are you?"
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
