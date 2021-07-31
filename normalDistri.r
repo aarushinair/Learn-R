f8e8bef974e1728cac18bb1701ee61b9bdc375f8
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:49:49 +0530

    Create normalDistri.r

---
+++ normalDistri.r
@@ -0,0 +1,22 @@
+#Normal Distribution
+> rnorm(1)    # generates 1 random number
+[1] 1.072712
+> rnorm(3)    # generates 3 random number
+[1] -1.1383656  0.2016713 -0.4602043
+> rnorm(3, mean=10, sd=2)    # provide our own mean and standard deviation
+[1]  9.856933  9.024286 10.822507
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
