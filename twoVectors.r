62c6d68fce639bb9648da64dc298118bed70864d
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:46:28 +0530

    Create twoVectors.r

---
+++ twoVectors.r
@@ -0,0 +1,30 @@
+#Add Two Vectors
+> x
+[1] 3 6 8
+> y
+[1] 2 9 0
+> x + y
+[1]  5 15  8
+> x + 1    # 1 is recycled to (1,1,1)
+[1] 4 7 9
+> x + c(1,4)    # (1,4) is recycled to (1,4,1) but warning issued
+[1]  4 10  9
+Warning message:
+In x + c(1, 4) :
+longer object length is not a multiple of shorter object length
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
