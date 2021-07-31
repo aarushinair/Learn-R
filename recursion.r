98fdd180ab2bfb4121c19fd56b36fcc4a9c0f531
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 18:29:39 +0530

    Create recursion.r

---
+++ recursion.r
@@ -0,0 +1,23 @@
+#Find Factorial of a number using recursion
+recur_factorial <- function(n) {
+if(n <= 1) {
+return(1)
+} else {
+return(n * recur_factorial(n-1))
+}
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
