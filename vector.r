f118c95fb6751886cc0c06933937dea33d0f1f9d
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:47:15 +0530

    Create vector.r

---
+++ vector.r
@@ -0,0 +1,28 @@
+#Vector Elements Arithmetic
+> sum(2,7,5)
+[1] 14
+> x
+[1]  2 NA  3  1  4
+> sum(x)    # if any element is NA or NaN, result is NA or NaN
+[1] NA
+> sum(x, na.rm=TRUE)    # this way we can ignore NA and NaN values
+[1] 10
+> mean(x, na.rm=TRUE)
+[1] 2.5
+> prod(x, na.rm=TRUE)
+[1] 24
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
