9d8703bda2f7d5c45ab7b2e948d0344eead72ba4
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:48:42 +0530

    Create uniformDistri.r

---
+++ uniformDistri.r
@@ -0,0 +1,7 @@
+#Uniform Distribution
+> runif(1)    # generates 1 random number
+[1] 0.3984754
+> runif(3)    # generates 3 random number
+[1] 0.8090284 0.1797232 0.6803607
+> runif(3, min=5, max=10)    # define the range between 5 and 10
+[1] 7.099781 8.355461 5.173133
