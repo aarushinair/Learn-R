2fa11e0855fce2d2fba806a722404c92c10153d2
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 18:01:29 +0530

    Create positiveNegative.r

---
+++ positiveNegative.r
@@ -0,0 +1,26 @@
+# In this program, we input a number check if the number is positive or negative or zero
+num = as.double(readline(prompt="Enter a number: "))
+if(num > 0) {
+print("Positive number")
+} else {
+if(num == 0) {
+print("Zero")
+} else {
+print("Negative number")
+}
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
