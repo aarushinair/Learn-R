80f48da98b73d079940090056eb1a2828be4238d
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-31 17:38:46 +0530

    Create 2linearRegression.r

---
+++ 2linearRegression.r
@@ -0,0 +1,31 @@
+#Perform linear regressing on the builtin cars dataset for the expression
+dist = Intercept + (β ∗ speed)
+
+
+head(cars)  
+scatter.smooth(x=cars$speed, y=cars$dist, main="Dist ~ Speed")  
+par(mfrow=c(1, 2))  
+boxplot(cars$speed, main="Speed", sub=paste("Outlier rows: ", boxplot.stats(cars$speed)$out))  
+boxplot(cars$dist, main="Distance", sub=paste("Outlier rows: ", boxplot.stats(cars$dist)$out))  
+#Density plot – Check if the response variable is close to normality
+library(e1071)
+par(mfrow=c(1, 2))  
+plot(density(cars$speed), main="Density Plot: Speed", ylab="Frequency", sub=paste("Skewness:", round(e1071::skewness(cars$speed), 2)))  
+polygon(density(cars$speed), col="red")
+plot(density(cars$dist), main="Density Plot: Distance", ylab="Frequency", sub=paste("Skewness:", round(e1071::skewness(cars$dist), 2)))  
+polygon(density(cars$dist), col="red")
+cor(cars$speed, cars$dist)  
+linearMod <- lm(dist ~ speed, data=cars)  
+print(linearMod)
+summary(linearMod)
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
