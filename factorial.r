9cef4c9923b99666779affe3a407d387f95612eb
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-08 19:26:07 +0530

    Update factorial.r

--- factorial.r
+++ factorial.r
@@ -20,7 +20,6 @@ print(paste("The factorial of", num ,"is",factorial))
 
 
 
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
