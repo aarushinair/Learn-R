d346bef096177e888d923e0937dec15685851bbf
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-09 11:08:01 +0530

    Update calculator.r

--- calculator.r
+++ calculator.r
@@ -32,6 +32,7 @@ print(paste(num1, operator, num2, "=", result))
 
 
 
+
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
