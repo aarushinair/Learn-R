457f30b3cb009208282e56f8e3cf4b44d810ef33
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-09 11:07:03 +0530

    Update 2linearRegression.r

--- 2linearRegression.r
+++ 2linearRegression.r
@@ -22,6 +22,9 @@ summary(linearMod)
 
 
 
+
+
+
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
