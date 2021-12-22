f5e845e6701e8d06a3a727e22ac34650fd1433a5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-12-22 12:01:29 +0530

    Update leapYear.r

--- leapYear.r
+++ leapYear.r
@@ -20,8 +20,6 @@ print(paste(year,"is not a leap year"))
 
 
 
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
