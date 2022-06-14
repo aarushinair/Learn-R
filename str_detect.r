ddf118b1fe1a6acfee15a48f1f75fbc1c96e86b5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-06-14 21:04:28 +0530

    Update str_detect.r

--- str_detect.r
+++ str_detect.r
@@ -13,3 +13,18 @@ str_detect(x, pat, negate = TRUE )
 #Output:
 #[1]  TRUE FALSE FALSE FALSE
 #[2]  FALSE TRUE TRUE TRUE
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
