7bbc1181f350390322901341c47a06ea74ff3e48
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-06-17 07:18:37 +0530

    Update checkUppercaseLowercase.r

--- checkUppercaseLowercase.r
+++ checkUppercaseLowercase.r
@@ -10,3 +10,18 @@ str_detect(t,"[[:lower:]]")
 #[1] "A"  "b"  "Cc" "D" 
 #[1]  TRUE FALSE  TRUE  TRUE
 #[1] FALSE  TRUE  TRUE FALSE
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
