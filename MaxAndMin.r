675d8b59351768b623429307aa72f3b3d3dda10f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-10-05 11:09:07 +0530

    Update MaxAndMin.r

--- MaxAndMin.r
+++ MaxAndMin.r
@@ -12,10 +12,6 @@
 [1]  2 10
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
