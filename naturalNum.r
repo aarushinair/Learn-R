2b967b3abd2e62affc080569142b4468cff09b8e
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-02-01 19:41:31 +0530

    Update naturalNum.r

--- naturalNum.r
+++ naturalNum.r
@@ -11,8 +11,7 @@ return(n + calculate_sum(n-1))
 
 
 
-
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -20,3 +19,4 @@ return(n + calculate_sum(n-1))
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
